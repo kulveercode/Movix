ecf278a7e9aef57df85cf176974970aaabcd741b
Kulveer Singh <kulveer.coder@gmail.com>
2023-08-30 11:30:40 +0530

    6 commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,20 @@ function App() {
   console.log(url);
 
   useEffect (() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi('/movie/popular')
+  const fetchApiConfig = () => {
+    fetchDataFromApi('/configuration')
     .then((res) => {
       console.log(res);
+
+      const url = {
+        backdrop : res.images.secure_base_url + "original",
+        poster : res.images.secure_base_url + "original",
+        profile : res.images.secure_base_url + "original",
+      }
+
       dispatch(getApiConfiguration(res));
     });
   };
