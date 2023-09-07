e9091f16f5e6d132ad70369498e764e6f1f4c0d4
Kulveer Singh <kulveer.coder@gmail.com>
2023-09-07 17:23:42 +0530

    Top rated section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import "./style.scss";
 
+import useFetch from "../../hooks/useFetch";
+
 const Details = () => {
+  // const { mediaType, id } = useParams();
+  // const {data, loading} = useFetch(`/${mediaType}/${id}`)
+
   return (
     <div>
-      
+      details
     </div>
   )
 }
